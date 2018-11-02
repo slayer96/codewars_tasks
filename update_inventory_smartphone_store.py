efec743c0955c8cff9e40243fde04dd4121d984f
Mykhailo Mykytyn <mykhailo.m@zazmic.com>
2018-11-02 19:25:27 +0200

    add solution for  update_inventory_smartphone_store

--- update_inventory_smartphone_store.py
+++ update_inventory_smartphone_store.py
@@ -9,7 +9,16 @@ Your task is to write a function that returns the updated list of your current i
 
 
 def update_inventory(cur_stock, new_stock):
-    pass
+    cur_stock = cur_stock + new_stock
+    results = {}
+    for cur in cur_stock:
+        if cur[-1] not in results.keys():
+            results[cur[1]] = cur[0]
+        else:
+            results[cur[1]] += cur[0]
+    results = [(i[1], i[0]) for i in results.items()]
+    results.sort(key=lambda x: x[1])
+    return results
 
 
 current = [(25, 'HTC'), (1000, 'Nokia'), (50, 'Samsung'), (33, 'Sony'), (10, 'Apple')]
