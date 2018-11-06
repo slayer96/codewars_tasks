91f0ad17023c888e735d45b29abe3d57040c773c
Mykhailo Mykytyn <mykhailo.m@zazmic.com>
2018-11-06 23:04:08 +0200

    add solution(working for current tests)

--- whats_the_degree.py
+++ whats_the_degree.py
@@ -16,20 +16,26 @@ use integers to get exact results without rounding errors
 import nose.tools as test
 
 
-def degree(p):
+def polyn(p, base):
     count = 0
-    base = 2
     result = p(base)
-    print(result, base * base * base)
-
+    res_0 = p(0)
+    result = result - res_0
     tmp = base
     while True:
         if tmp > result:
-            return 0
+            return count
         if tmp == result:
             return count + 1
         tmp = tmp * base
         count += 1
+
+
+def degree(p):
+    res = []
+    for i in range(6, 11):
+        res.append(polyn(p, i))
+    return sum(res) // len(res)
 
 
 test.assert_equals(degree(lambda x: 42), 0)
@@ -45,3 +51,5 @@ test.assert_equals(degree(lambda x: x * x * x * x), 4)
 test.assert_equals(degree(lambda x: x * x * x * x * x), 5)
 
 test.assert_equals(degree(lambda x: 5 + 3 * x + 2 * x * x), 2)
+
+test.assert_equals(degree(lambda x: x ** 7), 7)
